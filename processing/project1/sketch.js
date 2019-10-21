c5ccd1359c5af808b05a4eff2e4df03dba3dcd5e
Carlos Jauregui <carlos7adrian@yahoo.com>
2019-10-21 12:14:38 -0700

    update project1 sketch

--- processing/project1/sketch.js
+++ processing/project1/sketch.js
@@ -3,10 +3,14 @@ function setup() {
 }
 
 function draw() {
+  let color_x = windowWidth / 255
+  let color_y = windowHeight / 255
+  let color_xy = (sqrt(pow(windowWidth, 2) + pow(windowHeight, 2))) / 255
+  let mouse_xy = sqrt(pow(mouseX, 2) + pow(mouseY, 2))
   if(mouseIsPressed){
-    fill(mouseX / 3, mouseY / 2, 85);
-  } else {
     fill(255);
+  } else {
+    fill(floor(mouseX / color_x), floor(mouseY / color_y), floor(mouse_xy / color_xy));
   }
   ellipse(mouseX, mouseY, 80, 80);
 }
