12f2a1393f06cebb23841c3050e90518cef39976
Carlos Jauregui <carlos7adrian@yahoo.com>
2019-10-21 17:31:48 -0700

    embed python programs

--- processing/project1/sketch.js
+++ processing/project1/sketch.js
@@ -6,11 +6,11 @@ function draw() {
   let color_x = windowWidth / 255
   let color_y = windowHeight / 255
   let color_xy = (sqrt(pow(windowWidth, 2) + pow(windowHeight, 2))) / 255
-  let mouse_xy = sqrt(pow(mouseX, 2) + pow(mouseY, 2))
+  let mouse_xy = (mouseX + mouseY) / 2
   if(mouseIsPressed){
     fill(255);
   } else {
-    fill(floor(mouseX / color_x), floor(mouseY / color_y), floor(mouse_xy / color_xy));
+    fill(floor(mouseX / color_x), floor(mouse_xy / color_xy), floor(mouseY / color_y));
   }
   ellipse(mouseX, mouseY, 80, 80);
 }
