42b721249039670f77a6e0c26aeb1645cba39065
Carlos Jauregui <carlos7adrian@yahoo.com>
2019-10-23 16:26:08 -0700

    Add processing project two

--- processing/project2/sketch.js
+++ processing/project2/sketch.js
@@ -1,7 +1,31 @@
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  var g = 15;
+  color_r = 0
+  color_g = 0
+  color_b = 0
 }
 
+
+
 function draw() {
-  // put drawing code here
+  color_r = color_r + 1
+  color_g = color_g + 2
+  color_b = color_b + 3
+
+  if (color_r > 250){
+    color_r = 0
+  }
+  if (color_g > 250){
+    color_g = 0
+  }
+  if (color_b > 250){
+    color_b = 0
+  }
+  rectMode(CENTER);
+  translate(mouseX / mouseX, mouseY / mouseY);
+  translate(p5.Vector.fromAngle(millis() / 100, 80));
+  fill(color_r, color_g, color_b)
+  rect(mouseX, mouseY, 30, 30);
 }
